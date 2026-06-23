023e897b5ad2fd33be42091f0f2d8e94e3f6058e
tipstech-exe <55263599+mpsps@users.noreply.github.com>
2026-06-22 23:31:26 -0300

    código comentado e organizado

--- tipstech/views.py
+++ tipstech/views.py
@@ -17,14 +17,14 @@ def servicos(request):
     return render(request, 'servicos.html')
 
 def noticias(request):
+ # API do canal techbr
     feed = feedparser.parse(
         'https://feeds.feedburner.com/canaltechbr'
     )
-
+# fitro por plavras chaves relacionadas à tecnologias
     palavras_chave = [
         'hardware',
         'software',
-        'segurança',
         'cybersecurity',
         'android',
         'python',
@@ -34,7 +34,14 @@ def noticias(request):
         'django',
         'vue',
         'react',
-
+        'Sistemas Operacionais',
+        'Notebooks',
+        'Robótica',
+        'Internet das Coisas',
+        'Desenvolvimento Mobile',
+        'Big Data',
+        'Machine Learning',
+        'Redes de Computadores',
     ]
 
     noticias = []
@@ -44,7 +51,6 @@ def noticias(request):
             item.title + ' ' +
             item.get('summary', '')
         ).lower()
-
         if any(palavra.lower() in texto for palavra in palavras_chave):
             noticias.append({
                 'titulo': item.title,
@@ -52,54 +58,44 @@ def noticias(request):
                 'resumo': item.get('summary', ''),
                 'data': item.get('published', '')
             })
-
     return render(
         request,
         'noticias.html',
-        {'noticias': noticias[:25]}
+        {'noticias': noticias[:25]} # limite máximo de notícia
     )
 
 def projetos(request):
     return render(request, 'projetos.html') 
-
+# recebe o comentário do cliente
 def ok(request):
     if request.method == "POST":
         form = adicionarComentario(request.POST)
-
         if form.is_valid():
-
             nome = form.cleaned_data['nome']
             total = Comentarios.objects.filter(nome=nome).count()
-
             if total >= 3:
                 return JsonResponse({
                     "success": False,
                     "message": "Você atingiu o limite de 3 comentários."
                 })
-
             form.save()
-
             return JsonResponse({
                 "success": True,
                 "message": "Comentário enviado com sucesso!"
             })
-
         return JsonResponse({
             "success": False,
             "errors": form.errors
         })
-
     return JsonResponse({"success": False})
-
+# manda o a lista de comentários em json para a página
 def listar_comentarios(request):
     comentarios = []
-
     for c in Comentarios.objects.all().order_by('-id'):
         data_brasil = timezone.localtime(
             c.data_da_postagem,
             ZoneInfo("America/Fortaleza")
         )
-
         comentarios.append({
             'id': c.id,
             'nome': c.nome,
@@ -107,18 +103,16 @@ def listar_comentarios(request):
             'qtd_likes': c.qtd_likes,
             'data_da_postagem': data_brasil.strftime('%d/%m/%Y %H:%M')
         })
-
     return JsonResponse(comentarios, safe=False)
-
+# altera a qtd de curtida de um comentário 
 def curtir_comentario(request, id):
     comentario = Comentarios.objects.get(id=id)
-
     curtidos = request.session.get('comentarios_curtidos', [])
 
-    if id in curtidos:
+    if id in curtidos: #limita uma curtida por comentário
         return JsonResponse({
             'success': False,
-            'message': 'Você já curtiu este comentário.'
+            'message': 'Você já curtiu este comentário.' 
         })
 
     comentario.qtd_likes += 1
